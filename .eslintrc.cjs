e928176422cbce8a83e9afda53022ed319910a2a
sonsu95 <leesonsu07@gmail.com>
2024-10-05 00:05:26 +0900

    chore: update packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,16 +13,9 @@ module.exports = {
     project: './tsconfig.json',
   },
   rules: {
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-explicit-any': 'error',
-    'react/jsx-props-no-spreading': 'off',
-    'import/prefer-default-export': 'off',
+    '@typescript-eslint/lines-between-class-members': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
     'react/react-in-jsx-scope': 'off',
-    'react/forbid-prop-types': 'off',
-    'react/self-closing-comp': 'off',
-    'object-curly-newline': 'off',
-    'no-underscore-dangle': 'off',
-    'import/no-mutable-exports': 'off',
     'prettier/prettier': ['error'],
   },
   settings: {
