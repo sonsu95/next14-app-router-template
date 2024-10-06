875248308b3a7a627fda425cc575802a41ae1f52
sonsu95 <leesonsu07@gmail.com>
2024-10-07 00:16:23 +0900

    chore: update .eslint.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,23 +4,33 @@ module.exports = {
     'airbnb',
     'airbnb-typescript',
     'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   ignorePatterns: ['app/Providers.tsx'],
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   rules: {
     '@typescript-eslint/lines-between-class-members': 'off',
     '@typescript-eslint/no-throw-literal': 'off',
     'react/react-in-jsx-scope': 'off',
     'prettier/prettier': ['error'],
+    'import/prefer-default-export': 'off',
   },
   settings: {
     react: {
       version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {},
     },
   },
 };
